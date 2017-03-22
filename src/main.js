bcc30885aae1e09e6915cd1d06d85fd6e9721de2
matikrk <matikrk@gmail.com>
2017-03-22 17:07:14 +0100

    delete comment

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import  '../less/main.less'; // import styles and create style.css
 import Game from './Game';
 
-
-// eslint-disable-next-line
-console.log('Hello', Game);
-
 window.aaa = new Game();
