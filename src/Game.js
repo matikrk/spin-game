3e89878100bd6f477c77ff71a99d274c2c7393d0
matikrk <matikrk@gmail.com>
2017-03-23 15:31:00 +0100

    game engine win/loose

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,19 @@
 class Game {
   constructor() {
+    this.getUIReferences();
     this.loadResources();
+    this.attachListeners();
+  }
+
+  getUIReferences() {
+    const $ = id => document.getElementById(id);
+
+    this.elements = {
+      select: $('select'),
+      canvas: $('canvas'),
+      results: $('results'),
+      submit: $('submit'),
+    };
   }
 
   loadResources() {
@@ -17,17 +30,13 @@ class Game {
 
   prepareOptions(data) {
 
-    const select = document.getElementById('select');
-
     Object.keys(data).forEach(key => {
       const option = document.createElement('option');
       option.value = key;
       option.text = key;
 
-      select.appendChild(option)
+      this.elements.select.appendChild(option)
     });
-
-
 
     return data;
   }
@@ -60,6 +69,42 @@ class Game {
 
   stopLoading() {
     console.log('stopLoading');
+  }
+
+  attachListeners() {
+    this.elements.submit.addEventListener('click', this.onSubmit.bind(this))
+  }
+
+  onSubmit() {
+    const selected = this.elements.select.value;
+
+    this.spin(selected);
+  }
+
+  spin(selected) {
+    return new Promise(
+      resolve => {
+        const winnerIndex = Math.floor(Math.random() * this.loadedImages.length);
+        const winner = this.loadedImages[winnerIndex];
+
+
+        if (winner.name === selected) {
+          this.onWin();
+        } else {
+          this.onLoose();
+        }
+
+      }
+    );
+  }
+
+  onWin() {
+    this.elements.results.textContent = 'Win'
+  }
+
+  onLoose() {
+    this.elements.results.textContent = 'Loose'
+
   }
 
 }
