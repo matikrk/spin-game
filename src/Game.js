4024aee11dcdaff0bbeb2504ca3d6ed66770367f
matikrk <matikrk@gmail.com>
2017-03-23 15:05:46 +0100

    fill options

--- src/Game.js
+++ src/Game.js
@@ -7,10 +7,29 @@ class Game {
     this.startLoading();
 
     const symbolsPath = '/rest/symbols.json';
+
     fetch(symbolsPath)
       .then(response => response.json())
+      .then(data => this.prepareOptions(data))
       .then(data => this.prepareImages(data))
       .then(() => this.stopLoading());
+  }
+
+  prepareOptions(data) {
+
+    const select = document.getElementById('select');
+
+    Object.keys(data).forEach(key => {
+      const option = document.createElement('option');
+      option.value = key;
+      option.text = key;
+
+      select.appendChild(option)
+    });
+
+
+
+    return data;
   }
 
   prepareImages(data) {
